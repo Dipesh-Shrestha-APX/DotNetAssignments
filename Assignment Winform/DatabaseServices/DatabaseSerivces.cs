cd7fd01f1b5dddd0bd6c07fe079b3c5dab67a77c
Dipesh <deepeshpixar123@gmail.com>
2025-06-05 07:10:36 +0545

    Assignment winform added

---
+++ Assignment Winform/DatabaseServices/DatabaseSerivces.cs
@@ -0,0 +1,48 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Form.DatabaseServices
+{
+    internal class DatabaseSerivces
+    {
+        private readonly string _codatabasennectionString;
+        public DatabaseServices() {
+            this._connectionString = "Server=localhost; port=5432; User Id = postgres; Password=dipesh#123; Database=dipesh_table";
+        }
+        public string load_register(Register register)
+        {
+            try
+            {
+                using (var connection = new NpgsqlConnection(_con+nectionString))
+                {
+                    connection.Open();
+                    using (var cmd = new NpgsqlCommand(
+                        @"INSERT INTO register (name, age, gender, province, district)
+                    VALUES (@name, @age, @gender, @province, @district)", connection))
+                    {
+                        cmd.Parameters.AddWithValue("@name", register.Name ?? (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@age", register.Age ?? (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@gender", register.Gender ?? (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@province", register.Province ?? (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@district", register.District ?? (object)DBNull.Value);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                return "Registration Successful.";
+            }
+            catch (NpgsqlException ex)
+            {
+                return $"Database error: {ex.Message}";
+            }
+            catch (Exception ex)
+            {
+                return $"An error occurred-: {ex.Message}";
+            }
+        }
+    }
+}
---
+++ Assignment Winform/Models/Register.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Form.Models
+{
+    internal class Register
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string Skills { get; set; }
+
+    }
+}
